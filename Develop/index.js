c3840c50d4c276d268e3f83e7ddbe5b1cbc1b085
Kieran Brobst <kieranbrobst@gmail.com>
2020-12-03 20:23:29 -0800

    added prompts

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,27 @@ const fs = require ("fs");
 const inquirer = require ("inquirer");
 
 inquirer.prompt([
-   {
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "usernameInput",
+    },
+    {
+        type: "input",
+        message: "What is the repo name?",
+        name: "repoInput",
+    },
+    {
+        type: "input",
+        message: "What is the URL of the depository?",
+        name: "urlInput",
+    },
+    {
+        type: "input",
+        message: "What is your email contact?",
+        name: "emailInput",
+    },
+    {
     type: "input",
     message: "What is the title of this project?",
     name: "titleInput",
@@ -15,13 +35,24 @@ inquirer.prompt([
    },
    {
     type: "input",
+    message: "What are the installation steps?",
+    name: "installInput",
+   },
+   {
+    type: "input",
     message: "What is the expected output?",
     name: "usageInput",
    },
    {
+    type: "list",
+    message: "What is the license for this application?",
+    name: "license",
+    choices: ["MIT", "GPLv2", "Apache", "Unlicensed"]
+   },
+   {
     type: "input",
-    message: "Do you have ideas for releases in the future?",
-    name: "roadInput",
+    message: "How to test the application?",
+    name: "testInput",
    },
    {
     type: "input",
@@ -34,28 +65,57 @@ inquirer.prompt([
     name: "contributeInput",
    },
 ])
-.then((res) => {
-    console.log(res);
+.then((response) => {
+    console.log(response);
 });
 
 
-const questions = [
 
-];
+// // function to write README file
+fs.writeFile("README.md", response, (err) => {
+    if (err) throw err;
+    console.log("Complete");
+})
 
-// function to write README file
-// fs.writeFile("README.md", readme, (err) => {
-//     if (err) throw err;
-//     console.log("Complete");
-// })
+// // mark down
 
-function writeToFile(fileName, data) {
-}
+// const readme = `
+// # ${response.titleInput}
 
-// function to initialize program
-function init() {
+// ## Table of Contents
+// * [Description](#description)
+// * [Installation](#installation)
+// * [Usage](#usage)
+// * [License](#license)
+// * [Contributors](#contributors)
+// * [Testing](#testing)
+// * [Questions](#questions)
 
-}
 
-// function call to initialize program
-init();
+// ## Descritption
+// ${response.descInput}
+
+// ## Installation
+// ${response.installInput}
+
+// ## Usage
+// ${response.usageInput}
+
+// ## License
+// This application is covered under the ${response.license} license.
+
+// ## Contributors
+// ${response.contributeInput}
+
+// ## Testing
+// ${response.testInput}
+
+// ## Questions
+// Created by [${response.usernameInput}](https://github.com/${response.usernameInput})
+
+// [${response.repoInput}] (${response.urlInput})
+
+// If you have any questions, please reach me at [${response.emailInput}](${response.emailInput})
+
+
+// `;
---
+++ demo.md
@@ -0,0 +1,38 @@
+# ${response.titleInput}
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+
+
+## Descritption
+${response.descInput}
+
+## Installation
+${response.installInput}
+
+## Usage
+${response.usageInput}
+
+## License
+This application is covered under the ${response.license} license.
+
+## Contributors
+${response.contributeInput}
+
+## Testing
+${response.testInput}
+
+## Questions
+Created by [${response.usernameInput}](https://github.com/${response.usernameInput})
+
+[${response.repoInput}] (${response.urlInput})
+
+If you have any questions, please reach me at [EMAIL](email link)
+
+
