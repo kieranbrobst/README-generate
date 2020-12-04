fc33bccd444257c322c785ce1bdd60d071ac2161
Kieran Brobst <kieranbrobst@gmail.com>
2020-12-03 16:55:01 -0800

    required fs and inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,34 @@
 // array of questions for user
+const fs = require ("fs");
+const inquirer = require ("inquirer");
+
+inquirer.prompt([
+   {
+    type: "input",
+    message: "What is the title of this project?",
+    name: "titleInput",
+   },
+   {
+    type: "input",
+    message: "What is the description of the project?",
+    name: "descInput",
+   },
+])
+.then((res) => {
+    console.log(res);
+});
+
+
 const questions = [
 
 ];
 
 // function to write README file
+// fs.writeFile("README.md", readme, (err) => {
+//     if (err) throw err;
+//     console.log("Complete");
+// })
+
 function writeToFile(fileName, data) {
 }
 
