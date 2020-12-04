90b0896317b78ba8d8c6b67476b11f7f5651ed82
Kieran Brobst <kieranbrobst@gmail.com>
2020-12-03 17:06:48 -0800

    added questions for readme

--- Develop/index.js
+++ Develop/index.js
@@ -13,6 +13,26 @@ inquirer.prompt([
     message: "What is the description of the project?",
     name: "descInput",
    },
+   {
+    type: "input",
+    message: "What is the expected output?",
+    name: "usageInput",
+   },
+   {
+    type: "input",
+    message: "Do you have ideas for releases in the future?",
+    name: "roadInput",
+   },
+   {
+    type: "input",
+    message: "Where can people find support?",
+    name: "supportInput",
+   },
+   {
+    type: "input",
+    message: "State if you are open to contributions and what your requirements are for accepting them",
+    name: "contributeInput",
+   },
 ])
 .then((res) => {
     console.log(res);
